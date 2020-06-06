d2db1b0a26cdec70e17b96041f12d754059f61ab
Ibrahim Kothawala <ibrahimmkothawala@gmail.com>
2020-06-06 00:25:27 -0400

    adding some plots of friction factor and another friction factor function

--- frictionFactor.py
+++ frictionFactor.py
@@ -5,6 +5,7 @@ Created on Mon Feb 24 15:17:00 2020
 @author: cemam
 """
 import numpy as np
+from scipy.optimize import fsolve
 #%%Functions
 #dh hydraulic diameter (m)
 #e roughness (m)
@@ -15,3 +16,44 @@ def f(dh,e,Re): #turbulent
     else: #laminar
         f = 64/Re
     return f
+
+def f_colebrook(dh,e,Re):
+    if Re > 2300:
+        def func(f_init):
+            return 1/np.sqrt(f_init) + 2*np.log(e/3.7/dh +2.51/Re/np.sqrt(f_init))
+        f_val = fsolve(func,f(dh,e,Re))
+    else: #laminar
+        f_val = 64/Re
+    return f_val
+    
+if __name__ == "__main__":
+
+    import matplotlib.pyplot as plt
+
+    e_range = np.logspace(-2,-6,7)
+    Re_range = np.logspace(1,8,100)
+    d = 1
+    funcs = [f,f_colebrook]
+    soln = np.zeros((len(funcs)+1,len(e_range),len(Re_range)))
+    for ii in range(len(funcs)):
+        for jj in range(len(e_range)):
+            for kk in range(len(Re_range)):
+                soln[ii,jj,kk] = funcs[ii](d,e_range[jj],Re_range[kk])
+#%%  
+    for ii in range(len(funcs)):
+        for jj in range(len(e_range)):
+            plt.loglog(Re_range,soln[ii,jj,:],'x',color = 'tab:olive')
+   
+    for jj in range(len(e_range)):
+        for kk in range(len(Re_range)):
+                soln[len(funcs),jj,kk] = np.average(soln[:,jj,kk])
+    for jj in range(len(e_range)):
+            plt.loglog(Re_range,soln[0,jj,:],'-', color ='black')
+    plt.savefig('friction factor.png')
+
+
+    
+
+
+
+# %%
