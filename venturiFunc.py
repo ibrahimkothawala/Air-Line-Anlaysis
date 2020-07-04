fcfdab59d2d405d1749aa06f0f356e181802ca97
Ibrahim Kothawala <ibrahimmkothawala@gmail.com>
2020-07-03 20:19:08 -0400

    created new file for venturi

---
+++ venturiFunc.py
@@ -0,0 +1,11 @@
+#author: Ibrahim Kothawala 
+
+import numpy as np
+
+#inputs 
+#outputs
+def PchokeCondition(gamma): #pressure ratio of Pthroat/Pdownstream to velocity choke flow
+    return (2/(gamma + 1))**(gamma/(gamma-1))
+
+#Outputs
+#inputs
