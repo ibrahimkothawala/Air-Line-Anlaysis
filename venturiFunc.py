3bfe600afa847c8b6797c8057fef4cda72d8ac62
Ibrahim Kothawala <ibrahimmkothawala@gmail.com>
2020-07-06 11:29:05 -0400

    Deleted unimportant files and added comments

--- venturiFunc.py
+++ venturiFunc.py
@@ -2,8 +2,9 @@
 
 import numpy as np
 
-#inputs 
-#outputs
+#inputs: gamma
+#outputs: Pstar/P0
+#References: https://en.wikipedia.org/wiki/Choked_flow
 def PchokeCondition(gamma): #pressure ratio of Pthroat/PdownstreamStagnation to velocity choke flow
     return (2/(gamma + 1))**(gamma/(gamma-1))
 
