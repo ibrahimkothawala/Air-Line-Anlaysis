be5cffec1d6bd2f40858380b2011922a254ffd0d
Ibrahim Kothawala <ibrahimmkothawala@gmail.com>
2020-07-06 11:54:04 -0400

    added some functions

--- venturiFunc.py
+++ venturiFunc.py
@@ -5,8 +5,24 @@ import numpy as np
 #inputs: gamma
 #outputs: Pstar/P0
 #References: https://en.wikipedia.org/wiki/Choked_flow
+#pg 601 eqn 15.46 Gas Dynamics James John 
 def PchokeCondition(gamma): #pressure ratio of Pthroat/PdownstreamStagnation to velocity choke flow
     return (2/(gamma + 1))**(gamma/(gamma-1))
 
-#Outputs
-#inputs
+#inputs: throat area, gamma, stagnation density, stagnation pressure, discharge coefficient
+#outputs: choked mass flow rate through throat area.
+#References: https://en.wikipedia.org/wiki/Choked_flow
+def chokedMassFlow(throatArea,gamma,rho_0,P0,c_d):
+    return c_d*throatArea*np.sqrt((gamma*rho_0*P0*(2/(gamma+1)))**((gamma+1)/(gamma-1)))
+
+#inputs: gamma mach number
+#outputs: P0/pStatic
+#reference: pg 75 eqn 3.15 Gas Dynamics James John
+def stagnationPressureRatio(gamma,mach):
+    return (1+(gamma-1)/2*mach*mach)**(gamma/(gamma-1))
+
+#inputs: stagnationPressureRatio, gamma
+#outputs: rhoStagnation/rhoStatic
+#reference: pg 76 right above eqn 3.17 Gas Dynamics James John
+def stagnationDensityRatio(stagnationPressureRatio,gamma):
+    return stagnationPressureRatio**(1/gamma)
